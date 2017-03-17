a76f0c1f4ead28eefe8008c330caf689b61ba8ae
Kevin Sjöberg <kev.sjoberg@gmail.com>
2017-03-17 13:55:33 +0100

    Remove configuration

--- lib/blacksmith.rb
+++ lib/blacksmith.rb
@@ -2,14 +2,6 @@ require 'blacksmith/version'
 require 'blacksmith/forge'
 require 'blacksmith/errors'
 require 'blacksmith/tooling'
-require 'blacksmith/configuration'
 
 module Blacksmith
-  def self.configure
-    yield configuration.builder
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new({})
-  end
 end
